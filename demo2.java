112bc7a0f2f8b1302f8ebccf30e2ec8373f54c4d
phuxiteo3 <74146862+phuxiteo3@users.noreply.github.com>
2021-09-30 21:52:40 +0700

    Add files via upload

---
+++ demo2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo;
+
+/**
+ *
+ * @author Dell
+ */
+public class demo2 {
+
+    public static void main(String[] args) {
+        int a;
+        int b;
+        a = 10;
+        b = 32;
+        int result = a + b;
+        if (result > 20) {
+            System.out.println(result);
+        } else {
+            System.out.println("Result <= 20");
+        }
+        String c = new String();
+    }
+}
